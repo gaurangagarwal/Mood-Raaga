4a815e0a6ff28fe5047faebddafa71addf77a4ac
Jatin Garg <2014csb1017@iitrpr.ac.in>
2015-05-12 21:34:27 +0530

    file added successfully

---
+++ 1.php
@@ -0,0 +1,592 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+Templify Template Version 1.1 for WYSIWYG Web Builder 9
+Change Log  -------------
+Version 1.1
+Removed Author Credit from Site HTML
+Version1.0
+Original ReleaseTemplify Template Version 1.1 for WYSIWYG Web Builder 9
+Change Log  -------------
+Version 1.1
+Removed Author Credit from Site HTML
+Version1.0
+Original Release
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Love Story</title>
+<meta name="generator" content="WYSIWYG Web Builder 10 - http://www.wysiwygwebbuilder.com">
+<style type="text/css">
+div#container
+{
+   width: 1250px;
+   position: relative;
+   margin: 0 auto 0 auto;
+   text-align: left;
+}
+body
+{
+   background-color: #98FB98;
+   background-image: url(images/1_bkgrnd.png);
+   background-repeat: repeat-x;
+   color: #000000;
+   font-family: Arial;
+   font-size: 13px;
+   margin: 0;
+   text-align: center;
+}
+</style>
+<link href="template3.css" rel="stylesheet" type="text/css">
+<link href="http://fonts.googleapis.com/css?family=Maven+Pro" rel="stylesheet" type="text/css">
+<style type="text/css">
+#masterframeLayer1
+{
+   background-color: #808080;
+}
+#indexLayer1
+{
+   background-color: #F0F8FF;
+   background: -moz-linear-gradient(bottom,#F0F8FF 0%,#A0A0FF 100%);
+   background: -webkit-linear-gradient(bottom,#F0F8FF 0%,#A0A0FF 100%);
+   background: -o-linear-gradient(bottom,#F0F8FF 0%,#A0A0FF 100%);
+   background: -ms-linear-gradient(bottom,#F0F8FF 0%,#A0A0FF 100%);
+   background: linear-gradient(bottom,#F0F8FF 0%,#A0A0FF 100%);
+}
+#wb_indexText3 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: left;
+}
+#wb_indexText3 div
+{
+   text-align: left;
+}
+#indexLayer3
+{
+   background-color: #87CEFA;
+   background: -moz-linear-gradient(bottom,#87CEFA 0%,#1E90FF 100%);
+   background: -webkit-linear-gradient(bottom,#87CEFA 0%,#1E90FF 100%);
+   background: -o-linear-gradient(bottom,#87CEFA 0%,#1E90FF 100%);
+   background: -ms-linear-gradient(bottom,#87CEFA 0%,#1E90FF 100%);
+   background: linear-gradient(bottom,#87CEFA 0%,#1E90FF 100%);
+}
+#indexRollOver1 a
+{
+   display: block;
+   position: relative;
+   height: 100%;
+   width: 100%;
+}
+#indexRollOver1 a img
+{
+   position: absolute;
+   z-index: 1;
+   border-width: 0;
+}
+#indexRollOver1 span
+{
+   display: block;
+   height: 100%;
+   width: 100%;
+   position: absolute;
+   z-index: 2;
+}
+#indexRollOver1 img
+{
+   border-width: 0;
+   position: absolute;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#indexRollOver2 a
+{
+   display: block;
+   position: relative;
+   height: 100%;
+   width: 100%;
+}
+#indexRollOver2 a img
+{
+   position: absolute;
+   z-index: 1;
+   border-width: 0;
+}
+#indexRollOver2 span
+{
+   display: block;
+   height: 100%;
+   width: 100%;
+   position: absolute;
+   z-index: 2;
+}
+#indexRollOver2 img
+{
+   border-width: 0;
+   position: absolute;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#indexRollOver4 a
+{
+   display: block;
+   position: relative;
+   height: 100%;
+   width: 100%;
+}
+#indexRollOver4 a img
+{
+   position: absolute;
+   z-index: 1;
+   border-width: 0;
+}
+#indexRollOver4 span
+{
+   display: block;
+   height: 100%;
+   width: 100%;
+   position: absolute;
+   z-index: 2;
+}
+#indexRollOver4 img
+{
+   border-width: 0;
+   position: absolute;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#indexRollOver6 a
+{
+   display: block;
+   position: relative;
+   height: 100%;
+   width: 100%;
+}
+#indexRollOver6 a img
+{
+   position: absolute;
+   z-index: 1;
+   border-width: 0;
+}
+#indexRollOver6 span
+{
+   display: block;
+   height: 100%;
+   width: 100%;
+   position: absolute;
+   z-index: 2;
+}
+#indexRollOver6 img
+{
+   border-width: 0;
+   position: absolute;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#page2Layer1
+{
+   background-color: transparent;
+   filter: alpha(opacity=88);
+   -moz-opacity: 0.88;
+   opacity: 0.88;
+}
+#page2Layer2
+{
+   background-color: #DCDCDC;
+   background: -moz-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: -webkit-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: -o-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: -ms-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+}
+#page2Layer3
+{
+   background-color: #DCDCDC;
+   background: -moz-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: -webkit-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: -o-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: -ms-linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+   background: linear-gradient(bottom,#DCDCDC 0%,#FFFFFF 100%);
+}
+#page2Layer4
+{
+   background-color: #FFFFFF;
+   background: -moz-linear-gradient(bottom,#FFFFFF 0%,#DCDCDC 100%);
+   background: -webkit-linear-gradient(bottom,#FFFFFF 0%,#DCDCDC 100%);
+   background: -o-linear-gradient(bottom,#FFFFFF 0%,#DCDCDC 100%);
+   background: -ms-linear-gradient(bottom,#FFFFFF 0%,#DCDCDC 100%);
+   background: linear-gradient(bottom,#FFFFFF 0%,#DCDCDC 100%);
+}
+#Jashn_e_bahara_8Shape1
+{
+   border-width: 0;
+}
+#Jashn_e_bahara_8Image1
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#Jashn_e_bahara_8Image2
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#page2YouTube1
+{
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#Jashn_e_bahara_8Image3
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#Jashn_e_bahara_8Image4
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#wb_masterframeText1 
+{
+   background-color: transparent;
+   border: 0px #000000 solid;
+   padding: 0;
+   margin: 0;
+   text-align: left;
+}
+#wb_masterframeText1 div
+{
+   text-align: left;
+}
+#masterframeImage3
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+#wb_masterframeCssMenu1
+{
+   border: 0px #C0C0C0 solid;
+   background-color: transparent;
+}
+#wb_masterframeCssMenu1 ul
+{
+   list-style-type: none;
+   margin: 0;
+   padding: 0;
+   position: relative;
+   display: inline-block;
+}
+#wb_masterframeCssMenu1 li
+{
+   float: left;
+   margin: 0;
+   padding: 0px 10px 0px 0px;
+}
+#wb_masterframeCssMenu1 a
+{
+   display: block;
+   float: left;
+   color: #00008B;
+   border: 2px #FFFFFF outset;
+   -moz-border-radius: 10px;
+   -webkit-border-radius: 10px;
+   border-radius: 10px;
+   background-color: #FFFFFF;
+   background-image: none;
+   font-family: "Maven Pro";
+   font-size: 20px;
+   font-weight: normal;
+   font-style: normal;
+   text-decoration: none;
+   height: 27px;
+   line-height: 27px;
+   padding: 0px 9px 0px 9px;
+   vertical-align: middle;
+   text-align: center;
+}
+#wb_masterframeCssMenu1 li:hover a, #wb_masterframeCssMenu1 a:hover, #wb_masterframeCssMenu1 .active
+{
+   color: #FFFFFF;
+   background-color: #FF4500;
+   background-image: none;
+   border: 2px #FF0000 outset;
+}
+#wb_masterframeCssMenu1 li.firstmain
+{
+   padding-left: 0px;
+}
+#wb_masterframeCssMenu1 li.lastmain
+{
+   padding-right: 0px;
+}
+#wb_masterframeCssMenu1 br
+{
+   clear: both;
+   font-size: 1px;
+   height: 0;
+   line-height: 0;
+}
+#masterframeImage4
+{
+   border: 0px #000000 solid;
+   left: 0;
+   top: 0;
+   width: 100%;
+   height: 100%;
+}
+</style>
+<script type="text/javascript" src="swfobject.js"></script>
+<script type="text/javascript" src="jquery-1.11.1.min.js"></script>
+<script type="text/javascript" src="jquery.2dtransform.js"></script>
+<script type="text/javascript" src="jquery.ui.effect.min.js"></script>
+<script type="text/javascript" src="fancybox/jquery.easing-1.3.pack.js"></script>
+<link rel="stylesheet" href="fancybox/jquery.fancybox-1.3.0.css" type="text/css">
+<script type="text/javascript" src="fancybox/jquery.fancybox-1.3.0.pack.js"></script>
+<script type="text/javascript" src="fancybox/jquery.mousewheel-3.0.2.pack.js"></script>
+<script type="text/javascript" src="wwb10.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function()
+{
+   $("#indexRollOver1 a").hover(function(e)
+   {
+      $("#indexRollOver1 span").animate({rotate: 180, scale: 10, opacity: "hide"}, {duration: 100, easing: 'linear'});
+      $("#indexRollOver1 .hover").css({scale: 10, rotate: (-180)});
+      $("#indexRollOver1 .hover").animate({opacity: "show", rotate: 0, scale: 1}, {duration: 100, easing: 'linear'});
+   }, function()
+   {
+      $("#indexRollOver1 .hover").animate({rotate: -180, scale: 10, opacity: "hide"}, {duration: 100, easing: 'linear'});
+      $("#indexRollOver1 span").css({scale: 10, rotate: (180)});
+      $("#indexRollOver1 span").animate({opacity: "show", rotate: 0, scale: 1}, {duration: 100, easing: 'linear'});
+   });
+   $("#indexRollOver2 a").hover(function(e)
+   {
+      $(".hover", this).css("top","100%");
+      $(".hover", this).css("zIndex","3");
+      $(".hover", this).stop().animate({top:"0px"},{queue:false,duration:100});
+   }, function()
+   {
+      $(".hover", this).stop().animate({top:"100%"},{queue:false,duration:100});
+   });
+   $("#indexRollOver4 a").hover(function(e)
+   {
+      $(".hover", this).css("top","100%");
+      $(".hover", this).css("zIndex","3");
+      $(".hover", this).stop().animate({top:"0px"},{queue:false,duration:100});
+   }, function()
+   {
+      $(".hover", this).stop().animate({top:"100%"},{queue:false,duration:100});
+   });
+   $("#indexRollOver6 a").hover(function(e)
+   {
+      $(".hover", this).css("top","100%");
+      $(".hover", this).css("zIndex","3");
+      $(".hover", this).stop().animate({top:"0px"},{queue:false,duration:100});
+   }, function()
+   {
+      $(".hover", this).stop().animate({top:"100%"},{queue:false,duration:100});
+   });
+});
+</script>
+<script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Maven+Pro:400,900,700:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script><!-- Insert Google Analystics code here -->
+</head>
+<body>
+   <div id="indexLayer3" style="position:absolute;text-align:center;left:0%;top:1070px;width:100%;height:38px;z-index:12;" title="Footer">
+      <div id="indexLayer3_Container" style="width:1260px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+         <div id="wb_indexText3" style="position:absolute;left:210px;top:10px;width:842px;height:16px;z-index:8;text-align:left;">
+            <span style="color:#000000;font-family:tahoma;font-size:13px;"><strong>Copyright © 2014 by &quot;Mood Raaga&quot;&nbsp;&nbsp; &#0149;&#8226;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp; All Rights reserved&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp; &#8226;&nbsp;&nbsp; &nbsp;&nbsp; Developed by a group of students from IIT&nbsp; Ropar</strong></span></div>
+      </div>
+   </div>
+   <div id="container">
+      <div id="page2Layer2" style="position:absolute;text-align:left;left:0px;top:253px;width:290px;height:410px;z-index:14;">
+         <div id="wb_Jashn_e_bahara_8Image3" style="position:absolute;left:190px;top:0px;width:100px;height:410px;z-index:10;">
+            <img src="images/img0110.jpg" id="Jashn_e_bahara_8Image3" alt=""></div>
+      </div>
+      <div id="page2Layer3" style="position:absolute;text-align:left;left:965px;top:253px;width:295px;height:410px;z-index:15;">
+      </div>
+      <div id="page2Layer4" style="position:absolute;text-align:left;left:0px;top:663px;width:1260px;height:65px;z-index:16;">
+        </div>
+      <div id="wb_Jashn_e_bahara_8Image1" style="position:absolute;left:0px;top:143px;width:495px;height:458px;z-index:17;">
+         <img src="images/music-notes-mhp-right.png" id="Jashn_e_bahara_8Image1" alt="">
+      </div>
+      <div id="wb_Jashn_e_bahara_8Image2" style="position:absolute;left:974px;top:163px;width:286px;height:262px;z-index:18;">
+         <img src="images/img0111.png" id="Jashn_e_bahara_8Image2" alt="">
+      </div>
+      <div id="wb_page2YouTube1" style="position:absolute;left:290px;top:253px;width:680px;height:410px;z-index:19;">
+         <iframe id="page2YouTube1" src="http://www.youtube.com/embed/8xg3vE8Ie_E?rel=1&amp;border=1&amp;loop=1&amp;autoplay=1&amp;version=3&amp;ap=%2526fmt%3D18&amp;autohide=1&amp;fs=1&amp;theme=dark" allowfullscreen=""></iframe>
+      
+	   <?php
+       //php code........................................................................................... 
+		 
+		if (isset($_GET['submitFmood'])) {
+		
+		
+		$con=mysqli_connect("mysql114.000webhost.com","a9151661_root","jatin11","a9151661_mydb");
+// Check connection
+if (mysqli_connect_errno()) {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+		} 
+		
+		$link = mysqli_real_escape_string($con, $_REQUEST['link']);
+		$fmood = mysqli_real_escape_string($con, $_REQUEST['fmood']);
+		$songID = mysqli_real_escape_string($con, $_REQUEST['songID']);
+		$Imood = mysqli_real_escape_string($con, $_REQUEST['Imood']);
+		 
+	   echo "<font size= 5px color='blue'><b>How effective was the song?<br>";
+echo "<a href='{$link}?high=true&fmood={$fmood}&Imood={$Imood}&submitFmood=true&songID={$songID}&link={$link}'>High</a>"	;
+echo "&nbsp&nbsp&nbsp&nbsp"; 	  
+echo "<a href='{$link}?medium=true&fmood={$fmood}&Imood={$Imood}&submitFmood=true&songID={$songID}&link={$link}'>Medium</a>";
+echo "&nbsp&nbsp&nbsp&nbsp"; 	
+echo "<a href='{$link}?low=true&fmood={$fmood}&Imood={$Imood}&submitFmood=true&songID={$songID}&link={$link}'>Low</a></font></b><br><br><br>";
+	   
+		//update of songs
+		 if (isset($_GET['high'])) {
+    
+  
+
+$sql = "UPDATE songs SET {$fmood} = {$fmood}+1  WHERE ID = {$songID}";
+if (mysqli_query($con, $sql)) {
+   
+}
+  }
+  
+  if (isset($_GET['medium'])) {
+    
+$sql = "UPDATE songs SET {$fmood} = {$fmood} WHERE ID = {$songID}";
+if (mysqli_query($con, $sql)) {
+   
+}
+  }
+  
+  if (isset($_GET['low'])) {
+     $playsong[$fmood]= $playsong[$fmood] -1;
+  $update = $playsong[$fmood];
+$sql = "UPDATE songs SET {$fmood} = {$fmood}-1 WHERE ID = {$songID}";
+if (mysqli_query($con, $sql)) {
+   
+}
+  }
+		
+		
+		
+		 echo "<font size= 5px color='blue'><b>Other Songs:-<br>";
+		$sort = mysqli_query($con,"SELECT * FROM songs ORDER BY $fmood-$Imood DESC");
+		$count=0;
+		while($topsongs = mysqli_fetch_array($sort)){
+         
+		if ($count < 6){
+		 $count = $count +1;
+		 if ($topsongs['ID'] != $songID ){
+		echo "<font size= 5px color='blue'><b><a href='{$topsongs['link']}?fmood={$fmood}&Imood={$Imood}&submitFmood=true&songID={$topsongs['ID']}&link={$topsongs['link']}'>{$topsongs['SongName']}</a></font></b>";
+		 echo  "<br>";
+		 
+		 
+		 }
+		 }
+		 }
+		}
+		//php code.............................................................................................
+		 ?>
+		
+	  <br>
+	  <br>
+	  
+	  
+	  </div>
+      <div id="wb_Jashn_e_bahara_8Image4" style="position:absolute;left:970px;top:253px;width:100px;height:410px;z-index:20;">
+         <img src="images/img0112.jpg" id="Jashn_e_bahara_8Image4" alt="">
+      </div>
+   </div>
+   <div id="masterframeLayer1" style="position:absolute;text-align:left;left:0%;top:130px;width:100%;height:8px;z-index:21;">
+   </div>
+   <div id="indexLayer1" style="position:absolute;text-align:center;left:0%;top:0px;width:100%;height:130px;z-index:22;">
+      <div id="indexLayer1_Container" style="width:1260px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+         <div id="indexRollOver1" style="position:absolute;left:939px;top:0px;overflow:hidden;width:35px;height:34px;z-index:0">
+            <a href="http://www.facebook.com" target="_blank">
+               <img class="hover" alt="Facebook" title="www.facebook.com/Mood Raaga" src="images/facebook-g.png">
+               <span><img alt="Facebook" title="www.facebook.com/Mood Raaga" src="images/facebook-c.png"></span>
+            </a>
+         </div>
+         <div id="indexRollOver2" style="position:absolute;left:990px;top:0px;overflow:hidden;width:35px;height:34px;z-index:1">
+            <a href="">
+               <img class="hover" alt="" src="images/twitter-g.png">
+               <span><img alt="" src="images/twitter-c.png"></span>
+            </a>
+         </div>
+         <div id="indexRollOver4" style="position:absolute;left:1039px;top:0px;overflow:hidden;width:35px;height:34px;z-index:2">
+            <a href="">
+               <img class="hover" alt="" src="images/google-g.png">
+               <span><img alt="" src="images/google-c.png"></span>
+            </a>
+         </div>
+         <div id="indexRollOver6" style="position:absolute;left:1087px;top:0px;overflow:hidden;width:35px;height:34px;z-index:3">
+            <a href="">
+               <img class="hover" alt="" src="images/youtube-g.png">
+               <span><img alt="" src="images/youtube-c.png"></span>
+            </a>
+         </div>
+         <div id="wb_masterframeText1" style="position:absolute;left:20px;top:20px;width:516px;height:100px;z-index:4;text-align:left;">
+            <span style="color:#9932cc;font-family:'maven pro';font-size:85px;"><strong> MoodRaaga </strong></span></div>
+         <div id="wb_masterframeImage4" style="position:absolute;left:530px;top:0px;width:350px;height:130px;z-index:5;">
+            <img src="images/music-notes-clip-art-png-MUSIC.png" id="masterframeImage4" alt=""></div>
+         <div id="wb_masterframeCssMenu1" style="position:absolute;left:750px;top:90px;width:421px;height:31px;text-align:right;z-index:6;">
+            <ul>
+               <li class="firstmain"><a href="Home_page_2.html" target="_self">Home</a>
+               </li>
+               <li><a href="javascript:displaylightbox('./Login.html?improve the type of music played on mood raaga',{})" target="_self">Expert</a>
+               </li>
+               <li><a href="./Web_developers.html" target="_self">About</a>
+               </li>
+            </ul>
+            <br>
+         </div>
+         <div id="wb_masterframeImage3" style="position:absolute;left:1170px;top:0px;width:90px;height:90px;z-index:7;">
+            <img src="images/green-32.png" id="masterframeImage3" alt=""></div>
+      </div>
+   </div>
+   <div id="page2Layer1" style="position:absolute;text-align:center;left:0%;top:143px;width:100%;height:140px;z-index:23;">
+      <div id="page2Layer1_Container" style="width:1260px;position:relative;margin-left:auto;margin-right:auto;text-align:left;">
+         <div id="wb_Jashn_e_bahara_8Shape1" style="position:absolute;left:349px;top:20px;width:562px;height:83px;z-index:9;">
+            <img src="images/img0109.png" id="Jashn_e_bahara_8Shape1" alt="" style="width:562px;height:83px;"></div>
+      </div>
+   </div>
+</body>
+</html>
